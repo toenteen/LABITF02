da6ff9d51cc7806a81c3d82a9284b48ddfde0ab9
toenteen <71368747+toenteen@users.noreply.github.com>
2020-11-06 17:07:41 +0700

    Create shoto.php

---
+++ shoto.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Hello</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="alert alert-success" role="alert">
+            <div class="card">
+              <img class="card-img-top" src="img/doh4.png" />
+              <div class="card-body">
+                <div class="alert alert-success" role="alert">
+                  <h4 class="card-title">Izuku Midoriya</h4>
+                </div>
+                <p class="card-text">
+                  One For All: Izuku's Quirk, which was passed down to him by
+                  All Might. A combination of a Quirk that can be passed down to
+                  others and a Quirk that stockpiles power!
+                </p>
+                <p>
+                  <a href="#" class="btn btn-success">Read more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="alert alert-info" role="alert">
+            <div class="card">
+              <img class="card-img-top" src="img/doh5.webp" />
+              <div class="card-body">
+                <div class="alert alert-info" role="alert">
+                  <h4 class="card-title">Shoto Todoroki</h4>
+                </div>
+                <p class="card-text">
+                  Half-Cold Half-Hot: Shoto's Quirk is a combination of his
+                  mother's ice Quirk and Endeavor's fire Quirk!
+                </p>
+                <p>
+                  <a href="#" class="btn btn-info">Read more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="alert alert-danger" role="alert">
+            <div class="card">
+              <img class="card-img-top" src="img/doh6.jpg" />
+              <div class="card-body">
+                <div class="alert alert-danger" role="alert">
+                  <h4 class="card-title">Bakugou Katsuki</h4>
+                </div>
+                <p class="card-text">
+                  Katsuki employing his Quirk's versatility in his fighting
+                  style Explosion!!
+                </p>
+                <p>
+                  <a href="#" class="btn btn-danger">Read more</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Close container -->
+  </body>
+</html>
